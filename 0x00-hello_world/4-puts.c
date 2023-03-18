097cecf445adf02764512fb2252641cadd1e8c56
AmazingMaurice <amazingmaurice1359@gmail.com>
2023-03-17 23:59:49 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
-* Main -  A program that prints a line with puts function
+* main -  Entry
 * Return: 0 (success)
-**/
+*/
 int main(void)
+
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
