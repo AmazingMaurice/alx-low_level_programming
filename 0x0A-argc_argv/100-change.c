beeceb83d3674f6f2c304d369762a6bd27f6824b
AmazingMaurice <amazingmaurice1359@gmail.com>
2023-04-10 09:30:41 -0700

    correction

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,13 +5,12 @@
  * main - prints the minimum number of coins to
  * make change for an amount of money
  * @argc: number of arguments
+ * @argv: array of arguments
  * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-	int num;
-	int j;
-	int result;
+	int num, j, result;
 	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
