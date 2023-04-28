e10690d3db115a88d5148a819700cfc976a141d5
AmazingMaurice <amazingmaurice1359@gmail.com>
2023-04-28 15:13:37 -0700

    task 01

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
 
 /**
  * print_lists - Writing a function that prints all the elements of a list_t list.
- * @h: The list_t list.
+ * @h: singly linked list.
  * Return: the number of element on the list
  */
 size_t print_list(const list_t *h)
